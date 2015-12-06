c9b93d3b8af6f8fdf81e1b9628d6298c23b44531
ibrunoleal <ibrunoleal@gmail.com>
2015-12-06 17:12:20 -0300

    adicinado funcionalidade de graficos

--- RdPAtividade5/src/br/ufc/arida/bcl/rdp20152/atv5/ex1/Exercicio1.java
+++ RdPAtividade5/src/br/ufc/arida/bcl/rdp20152/atv5/ex1/Exercicio1.java
@@ -1,7 +1,13 @@
 package br.ufc.arida.bcl.rdp20152.atv5.ex1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealVector;
+
+import br.ufc.arida.bcl.rdp20152.atv5.graficos.GraficoDePontos2D;
+import br.ufc.arida.bcl.rdp20152.atv5.graficos.Ponto2D;
 
 public class Exercicio1 {
 
@@ -17,10 +23,17 @@ public class Exercicio1 {
 			yPreditos.setEntry(i, yi);
 		}
 
+		List<Ponto2D> pontos = new ArrayList<Ponto2D>();
 		System.out.println("Valor Label(t) -- Valor Predito");
 		for (int i = 0; i < yPreditos.getDimension(); i++) {
 			System.out.println(f.getData_A_testing_output().getEntry(i) + " -- " + yPreditos.getEntry(i));
+			Ponto2D pi = new Ponto2D(f.getData_A_testing_output().getEntry(i), yPreditos.getEntry(i));
+			pontos.add(pi);
 		}
+		
+		GraficoDePontos2D g1 = new GraficoDePontos2D("Titulo");
+		g1.adicionarPontos2D("labels x preditos", pontos);
+		g1.exibirGrafico();
 		
 		double mse = f.funcao_MSE(f.getData_A_testing_output(), yPreditos);
 		System.out.println("\nMean Squared Error(MSE):");
